e6cea037f16b056cbfd5a2cfd78a69bf121c7d67
Justin Heath <70263625+justcoder1@users.noreply.github.com>
2023-12-29 15:43:17 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+let test = ''
+
 updateBG = () => {
   const x = document.querySelector('body').style.backgroundColor;
   document.querySelector('body').style.backgroundColor = (x === 'lightgoldenrodyellow') ? 'azure' : 'lightgoldenrodyellow';
