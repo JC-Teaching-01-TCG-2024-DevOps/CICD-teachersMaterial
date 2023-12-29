526ca88b090f680ca40f02721165244879a82a64
Justin Heath <just.coder@outlook.com>
2023-12-29 15:57:30 +0000

    come on please

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const updateBG = () => {
-  const x = document.querySelector("body").style.backgroundColor;
-  document.querySelector("body").style.backgroundColor =
-    x === "lightgoldenrodyellow" ? "azure" : "lightgoldenrodyellow";
-};
+  const x = document.querySelector('body').style.backgroundColor
+  document.querySelector('body').style.backgroundColor =
+    x === 'lightgoldenrodyellow' ? 'azure' : 'lightgoldenrodyellow'
+}
 
-document.querySelector("#my-btn").addEventListener("click", updateBG);
+document.querySelector('#my-btn').addEventListener('click', updateBG)
