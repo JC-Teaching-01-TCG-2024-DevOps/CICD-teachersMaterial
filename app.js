503292c1a1d395115c2fd7300d9ae29178410d7c
Justin Heath <just.coder@outlook.com>
2023-12-29 21:33:46 +0000

    update

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
+// Function to change colour
 const updateBG = () => {
   const x = document.querySelector('body').style.backgroundColor
   document.querySelector('body').style.backgroundColor =
     x === 'lightgoldenrodyellow' ? 'azure' : 'lightgoldenrodyellow'
 }
 
-document.querySelector('#my-btn').addEventListener('click', updateBG)
+// function to add numbers
+export const calcResult = (value1, value2) => {
+  return value1 + value2
+}
 
-let strange
+// function for form submit
+const runCalc = (e) => {
+  e.preventDefault();
+  const noOne = document.querySelector('#noOne').value
+  const noTwo = document.querySelector('#noTwo').value
+  console.log(noOne, noTwo);
+  document.querySelector('#formResult').innerHTML = `Result is: ${calcResult(+noOne, +noTwo)}`
+  document.querySelector('#form1').reset();
+}
+
+
+
+document.querySelector('#my-btn').addEventListener('click', updateBG)
+document.querySelector('#form1').addEventListener('submit', runCalc)
---
+++ app.test.js
@@ -0,0 +1,10 @@
+import { calcResult } from './app';
+
+describe('calc test', () => {
+
+    test("checks the input numbers add up", () => {
+      expect(
+        calcResult
+        ).toBeDefined();
+    });
+})
