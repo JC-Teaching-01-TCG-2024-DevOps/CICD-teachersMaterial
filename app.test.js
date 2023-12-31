5400574c7784d6b355f5b7cbe29ea05fd0edae1a
Justin Heath <just.coder@outlook.com>
2023-12-31 14:18:20 +0000

    updates

--- app.test.js
+++ app.test.js
@@ -1,12 +1,12 @@
-const updateBackgroundColour = require("./src/app");
+const updateBackgroundColour = require('./src/app')
 
-test("checks function exists", () => {
-  expect(updateBackgroundColour).toBeDefined();
-});
+test('checks function exists', () => {
+  expect(updateBackgroundColour).toBeDefined()
+})
 
-test("checks function excutes", () => {
-  expect(updateBackgroundColour).toBeTruthy();
-});
+test('checks function excutes', () => {
+  expect(updateBackgroundColour).toBeTruthy()
+})
 
 // test("checks function returns a colour", () => {
 //   expect(updateBackgroundColour).toBeFalsy();
