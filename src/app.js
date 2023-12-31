4343a3962bd56d16abd1afa8c94032489cad23c9
Justin Heath <just.coder@outlook.com>
2023-12-31 14:13:56 +0000

    fingers crossed

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 // Function to change colour
-const update_background_colour = () => {
+const updateBackgroundColour = () => {
   const x = document.querySelector('body').style.backgroundColor
   document.querySelector('body').style.backgroundColor =
     x === 'lightgoldenrodyellow' ? 'azure' : 'lightgoldenrodyellow'
 }
+
+module.exports = updateBackgroundColour
